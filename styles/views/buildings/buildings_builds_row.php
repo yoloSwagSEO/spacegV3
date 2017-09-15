1f95c4e342035765da79ff077e9b56d2f90fea1b
Florian Maison <erosdevfr@gmail.com>
2017-09-15 22:06:11 +0200

    [FIX] Rendre la zone rouge clickable pour avoir accées aux informations avancer meme sans avoir les ressources pour construires

--- styles/views/buildings/buildings_builds_row.php
+++ styles/views/buildings/buildings_builds_row.php
@@ -4,7 +4,7 @@
 		<div class="build">{click}</div>
 		<div class="name"><a href="game.php?page=infos&gid={i}" title="{descriptions}" data-id="{i}" class="infoshow tooltipbat">{n}</a> </div>
 		<div class="niveau"><center>{nivel}</center></div>
-		<div class="nobuild" style="{nobuild}"><a href="#" class="tooltipbat" title="{descriptions}" data-id="{i}" ></a></div>
+		<div class="nobuild" style="{nobuild}"><a href="game.php?page=infos&gid={i}" class="infoshow tooltipbat" title="{descriptions}" data-id="{i}" ></a></div>
 	</div>
 	<div class="item_info" id="itemm_info_{i}" style="display:none;">
 {price}
