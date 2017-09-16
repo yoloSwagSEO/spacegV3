22b1a6b78096d276fcb2677cbcf3b58e5cfcad60
Florian Maison <erosdevfr@gmail.com>
2017-09-16 16:17:24 +0200

    [FIX] Modification des design sur les actions de la colony

--- styles/views/overview/overview_renameplanet.php
+++ styles/views/overview/overview_renameplanet.php
@@ -1,22 +1,26 @@
-<br />
-<div id="content">
-    <form action="game.php?page=overview&mode=renameplanet&pl={planet_id}" method="POST">
-    <table width=519>
-    <tr>
-        <td class="c" colspan=3>{ov_your_planet}</td>
-    </tr><tr>
-        <th>{ov_coords}</th>
-        <th>{ov_planet_name}</th>
-        <th>{ov_actions}</th>
-    </tr><tr>
-        <th>{galaxy_galaxy}:{galaxy_system}:{galaxy_planet}</th>
-        <th>{planet_name}</th>
-        <th><input type="submit" name="action" value="{ov_abandon_planet}"></th>
-    </tr><tr>
-        <th>{ov_planet_rename}</th>
-        <th><input type="text" name="newname" size=25 maxlength=20></th>
-        <th><input type="submit" name="action" value="{ov_planet_rename_action}"></th>
-    </tr>
-    </table>
-    </form>
+<div class="row">
+    <div class="col-md-12 bgLR bdDotted" id="actionColonyHeader">
+        <p>Action sur la colonie ({planet_name} - {galaxy_galaxy}:{galaxy_system}:{galaxy_planet})</p>
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-12 bgLR bdDotted" id="list-bat">
+        <form action="game.php?page=overview&mode=renameplanet&pl={planet_id}" method="POST">
+            <table width=519>
+                <tr>
+                    <th></th>
+                    <th></th>
+                    <th>{ov_actions}</th>
+                </tr><tr>
+                    <th></th>
+                    <th></th>
+                    <th><input type="submit" name="action" value="{ov_abandon_planet}"></th>
+                </tr><tr>
+                    <th>{ov_planet_rename}</th>
+                    <th><input type="text" name="newname" size=25 maxlength=20></th>
+                    <th><input type="submit" name="action" value="{ov_planet_rename_action}"></th>
+                </tr>
+            </table>
+        </form>
+    </div>
 </div>
