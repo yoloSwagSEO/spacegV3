d2e69577a3dce7e914eb124099dda27e39efd1bb
MacHookipa <florian@studiohookipa.com>
2017-09-18 15:58:44 +0200

    [MOD]Passage en 3D

--- includes/classes/class.FleetHelpers.php
+++ includes/classes/class.FleetHelpers.php
@@ -3,15 +3,17 @@ if(!defined('INSIDE')){ die(header ( 'location:../../' ));}
 
 class FleetHelpers {
 
-    static function is_discoverable($randarRange,$FleetX,$FleetY,$ColonyX,$ColonyY):bool {
-        if(self::fleet_distance($FleetX,$FleetY,$ColonyX,$ColonyY) <= $randarRange){
+    static function is_discoverabl($randarRange,$FleetX,$FleetY,$FleetZ,$ColonyX,$ColonyY,$ColonyZ):bool {
+        if(self::fleet_distance($FleetX,$FleetY,$FleetZ,$ColonyX,$ColonyY,$ColonyZ) <= $randarRange){
             return true;
         }else{
             return false;
         }
     }
 
-    static function fleet_distance($fleet1X,$fleet1Y,$fleet2X,$Fleet2Y){
-        return sqrt(($fleet1X - $fleet2X)**2 + ($Fleet2Y - $fleet1Y)**2);
+    static function fleet_distance($fleet1X,$fleet1Y,$fleet1Z,$fleet2X,$Fleet2Y,$Fleet2Z){
+        return sqrt(($fleet2X-$fleet1X)**2+($Fleet2Y-$fleet1Y)**2+($Fleet2Z-$fleet1Z)**2);
     }
+
+
 }
