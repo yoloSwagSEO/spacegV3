ec308a21507137694c75f50ae496b5e7d4df5915
MacHookipa <florian@studiohookipa.com>
2017-09-18 15:51:50 +0200

    [FIX]Distance entre 2 points [ADD] fonction de calcul de la distance entre 2 points

--- includes/classes/class.FleetHelpers.php
+++ includes/classes/class.FleetHelpers.php
@@ -3,12 +3,15 @@ if(!defined('INSIDE')){ die(header ( 'location:../../' ));}
 
 class FleetHelpers {
 
-    static function is_discoverable($randarRange,$fleetX,$FleetY,$ColonyX,$colonyY):bool {
-        if(sqrt(($fleetX - $ColonyX)**2 + ($colonyY - $FleetY)) <= $randarRange){
+    static function is_discoverable($randarRange,$FleetX,$FleetY,$ColonyX,$ColonyY):bool {
+        if(self::fleet_distance($FleetX,$FleetY,$ColonyX,$ColonyY) <= $randarRange){
             return true;
         }else{
             return false;
         }
     }
-    
+
+    static function fleet_distance($fleet1X,$fleet1Y,$fleet2X,$Fleet2Y){
+        return sqrt(($fleet1X - $fleet2X)**2 + ($Fleet2Y - $fleet1Y)**2);
+    }
 }
