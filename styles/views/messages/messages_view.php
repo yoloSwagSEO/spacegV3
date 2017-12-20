3d633e19d7a557ae8f24d46d3a3e2f0d13de2452
MacHookipa <florian@studiohookipa.com>
2017-12-20 15:09:47 +0100

    [FIX]Modification du CSS de la lectures des messages [FIX]Correction de l'ajax des messages [ADD]Ajout des bouton répondre et supprimer au messages

--- styles/views/messages/messages_view.php
+++ styles/views/messages/messages_view.php
@@ -1,12 +1,17 @@
 <div id="messageview">
 	<div class="contentPageNavi"></div>
-	<div id="header_message">
-		<span class="labelM">De :</span> {from}<br />
-		<span class="labelM">A :</span>	{to}<br />
-		<span class="labelM">Objet :</span> {objet}<br />
-		<span class="labelM">Date :</span> {time}
-	</div>
-	<div id="action"></div>
+    <div class="row" id="header_message" >
+        <div class="col-md-8">
+            <span class="labelM">De :</span> {from}<br />
+            <span class="labelM">A :</span>	{to}<br />
+            <span class="labelM">Objet :</span> {objet}<br />
+            <span class="labelM">Date :</span> {time}
+        </div>
+        <div id="action" class="col-md-4">
+            <a href="game.php?page=createFleet" class="btns btn-116-24 right">Répondre</a>
+            <a href="game.php?page=createFleet" class="btns btn-116-24 right">Supprimer</a>
+        </div>
+    </div>
 	<div id="message_corp">
 		{text}
 	</div>
