d6d205ba7645deba06fa963310862e3ebc14b22b
YoloSwagSeo <erosdevfr@gmail.com>
2018-01-07 11:24:44 +0100

    [MAJ]Mise au norme graphique de la pages entrepots

--- styles/views/entrepot/entrepot_body.php
+++ styles/views/entrepot/entrepot_body.php
@@ -1,20 +1,23 @@
-<script src="js/gouv.js"></script>
-<div id="content">
-	<div id="list-bat">
-            <div id="labelshipyard"><p>Entrepots</p></div>
-            <b>Metal: {ApourcentMetal}% - {metal}/{metalMax}</b>
-               <div id="metal_entrepot">
-                  <div style="width:{pourcentMetal}%;height:16px;background:red;"></div>               
-               </div>           
-               <b>Cristal: {ApourcentCristal}% - {cristal}/{cristalMax}</b>
-               <div id="cristal_entrepot">
-                   <div style="width:{pourcentCristal}%;height:16px;background:red;"></div>
-               </div>   
-               <b>Uradium: {ApourcentDeut}% - {deut}/{deutMax}</b>    
-               <div id="uradium_entrepot">
-                   <div style="width:{pourcentDeut}%;height:16px;background:red;"></div>               
-               </div>                  
-               {item} 
-	<div class="clear"></div>
-	</div>
+<script src="js/pages/entrepots.js"></script>
+<div class="row" id="rowBuilding">
+    <div class="col-md-12 bgLR " id="labelshipyard">
+        <p>Entrepots</p>
+    </div>
+    <div class="col-md-12 bgLR pd-10" id="buildlistingBatNew">
+        <b>Metal: {ApourcentMetal}% - {metal}/{metalMax}</b>
+        <div id="metal_entrepot">
+            <div style="width:{pourcentMetal}%;height:16px;background:red;"></div>
+        </div>
+        <b>Cristal: {ApourcentCristal}% - {cristal}/{cristalMax}</b>
+        <div id="cristal_entrepot">
+            <div style="width:{pourcentCristal}%;height:16px;background:red;"></div>
+        </div>
+        <b>Uradium: {ApourcentDeut}% - {deut}/{deutMax}</b>
+        <div id="uradium_entrepot">
+            <div style="width:{pourcentDeut}%;height:16px;background:red;"></div>
+        </div>
+    </div>
+    <div class="col-md-12 bgLR pd-10" id="list-bat">
+        {item}
+    </div>
 </div>
